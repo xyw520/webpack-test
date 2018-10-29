2a51275ba8677debcd455f2e10df12e1216207a0
xyw520 <412976055@qq.com>
2018-10-29 16:18:30 +0800

    分离css操作增加的笔记

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 
-import './css/main.css';
-// require('./css/main.css');
+// import './css/main.css';
+require('./css/main.css');
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./componet/show');
 
